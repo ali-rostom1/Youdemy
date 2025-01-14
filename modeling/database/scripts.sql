a731552ae8259a2f719baede0c306286ba83ad80
ali-rostom1 <ali.rostom220@gmail.com>
2025-01-14 18:40:42 +0100

    add category table

--- modeling/database/scripts.sql
+++ modeling/database/scripts.sql
@@ -8,3 +8,8 @@ CREATE TABLE Users(
     email VARCHAR(100) NOT NULL, role ENUM('student', 'teacher') NOT NULL, 
     status ENUM('active', 'suspended') DEFAULT 'active' 
 );
+CREATE TABLE Categories ( 
+	category_id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(50) NOT NULL, 
+    description VARCHAR(255) 
+);
