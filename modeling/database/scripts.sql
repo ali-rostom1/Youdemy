c6f986107127a1315c6e9bcb74ab88d55a592f02
ali-rostom1 <ali.rostom220@gmail.com>
2025-01-14 18:48:13 +0100

    fix role column use table enum(admin)

--- modeling/database/scripts.sql
+++ modeling/database/scripts.sql
@@ -4,8 +4,10 @@ use Youdemy;
 
 CREATE TABLE Users( 
 	user_id INT AUTO_INCREMENT PRIMARY KEY, 
-    username VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, 
-    email VARCHAR(100) NOT NULL, role ENUM('student', 'teacher') NOT NULL, 
+    username VARCHAR(50) NOT NULL, 
+    password VARCHAR(255) NOT NULL, 
+    email VARCHAR(100) NOT NULL, 
+    role ENUM('student', 'teacher') NOT NULL, 
     status ENUM('active', 'suspended') DEFAULT 'active' 
 );
 
@@ -46,3 +48,4 @@ CREATE TABLE Enrollment (
     FOREIGN KEY (user_id) REFERENCES Users(user_id), 
     FOREIGN KEY (course_id) REFERENCES Courses(course_id) 
 );
+ALTER TABLE Users MODIFY COLUMN role ENUM('student', 'teacher', 'admin') NOT NULL;
