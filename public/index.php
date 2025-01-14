2bec419aa266b8847487a7098e60ae0cf9ee8082
ali-rostom1 <ali.rostom220@gmail.com>
2025-01-14 20:33:54 +0100

    setup router in entry point

--- public/index.php
+++ public/index.php
@@ -1,14 +1,29 @@
 <?php
-
+    use App\Router;
     use Dotenv\Dotenv;
 
     require_once "../vendor/autoload.php";
     
-
+    //LOADING ENVIROMENT VARIABLES    
     $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
     $dotenv->load();
 
 
+    $router = new Router();
+
+
+    //ADDING ROUTES
+    $router->add("/",function(){
+       echo "hello"; 
+    });
+    $router->add("/login",function(){
+        echo "login";
+    });
+    
+    $requestedURI = $_SERVER["REQUEST_URI"];
+
+    //DISPATCHING REQUEST BASED ON ROUTES ADDED
+    $router->dispatch($requestedURI);
 
 
 
