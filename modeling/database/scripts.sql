d3ecc8de3fc5c3d644f7e99cbdcbfe5032f547c6
ali-rostom1 <ali.rostom220@gmail.com>
2025-01-14 18:37:15 +0100

    add user table

--- modeling/database/scripts.sql
+++ modeling/database/scripts.sql
@@ -2,3 +2,9 @@ create database Youdemy;
 
 use Youdemy;
 
+CREATE TABLE Users( 
+	user_id INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, 
+    email VARCHAR(100) NOT NULL, role ENUM('student', 'teacher') NOT NULL, 
+    status ENUM('active', 'suspended') DEFAULT 'active' 
+);
