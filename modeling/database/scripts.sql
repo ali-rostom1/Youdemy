ab378327f10658ff8db5edc335dd63cdf0d3640f
ali-rostom1 <ali.rostom220@gmail.com>
2025-01-14 18:41:06 +0100

    add courses table

--- modeling/database/scripts.sql
+++ modeling/database/scripts.sql
@@ -8,8 +8,21 @@ CREATE TABLE Users(
     email VARCHAR(100) NOT NULL, role ENUM('student', 'teacher') NOT NULL, 
     status ENUM('active', 'suspended') DEFAULT 'active' 
 );
+
+
 CREATE TABLE Categories ( 
 	category_id INT AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(50) NOT NULL, 
     description VARCHAR(255) 
+);
+
+
+CREATE TABLE Courses ( 
+	course_id INT AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(100) NOT NULL, description TEXT, 
+    content VARCHAR(255), 
+    category_id INT, 
+    teacher_id INT, 
+    FOREIGN KEY (teacher_id) REFERENCES Users(user_id),
+    Foreign Key (category_id) REFERENCES Categories(category_id)
 );
